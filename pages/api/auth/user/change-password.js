d10f9372de18fb7022006e988d09cad2569d0915
yseij <r0790156@student.thomasmore.be>
2022-02-10 16:19:43 +0100

    API-routes beschermen

---
+++ pages/api/auth/user/change-password.js
@@ -0,0 +1,18 @@
+import { getSession } from "next-auth/client";
+
+async function handler(req, res) {
+  if (req.method !== "PATCH") {
+    return;
+  }
+
+  const session = await getSession({
+    req: req,
+  });
+
+  if (!session) {
+    res.status(401).json({ message: "niet geauthenticeerd" });
+    return;
+  }
+}
+
+export default handler;
