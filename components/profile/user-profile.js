89924f29110932fad6c88e57970df7b72ec52cc3
yseij <r0790156@student.thomasmore.be>
2022-02-10 15:31:56 +0100

    Paginabewaking aan de clientzijde toevoegen (routebeveiliging)

--- components/profile/user-profile.js
+++ components/profile/user-profile.js
@@ -1,8 +1,29 @@
-import ProfileForm from './profile-form';
-import classes from './user-profile.module.css';
+import { useSession, getSession } from "next-auth/client";
+import { useEffect, useState } from "react";
+
+import ProfileForm from "./profile-form";
+import classes from "./user-profile.module.css";
 
 function UserProfile() {
-  // Redirect away if NOT auth
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedSession, setLoadedSession] = useState();
+
+  useEffect(() => {
+    getSession().then((session) => {
+      setLoadedSession(session);
+      if (!session) {
+        window.location.href = "/auth";
+      } else {
+        setIsLoading(false);
+      }
+    });
+  }, []);
+
+  const [session, loading] = useSession();
+
+  if (loading) {
+    return <p className={classes.profile}>Loading</p>;
+  }
 
   return (
     <section className={classes.profile}>
