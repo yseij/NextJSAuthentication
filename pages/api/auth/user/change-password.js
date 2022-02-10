e64b3ca1404bb82b5ea19e06c2b02445f925ba0d
yseij <r0790156@student.thomasmore.be>
2022-02-10 16:37:12 +0100

    De logica Wachtwoord wijzigen toevoegen

--- pages/api/auth/user/change-password.js
+++ pages/api/auth/user/change-password.js
@@ -1,5 +1,6 @@
 import { getSession } from "next-auth/client";
-
+import { connectToDatabase } from "../../../../lib/db";
+import { verifyPassword, hashPassword } from "../../../../lib/auth";
 async function handler(req, res) {
   if (req.method !== "PATCH") {
     return;
@@ -13,6 +14,47 @@ async function handler(req, res) {
     res.status(401).json({ message: "niet geauthenticeerd" });
     return;
   }
+
+  const userEmail = session.user.email;
+  const oldPassword = req.body.oldPassword;
+  const newPassword = req.body.newPassword;
+
+  const client = await connectToDatabase();
+
+  const usersCollection = client.db().collection("users");
+
+  const user = await usersCollection.findOne({ email: userEmail });
+
+  if (!user) {
+    res.status(401).json({ message: "user niet gevonden" });
+    return;
+  }
+
+  const currentPassword = user.password;
+
+  const passwordsAreEquel = await verifyPassword(oldPassword, currentPassword);
+
+  if (!passwordsAreEquel) {
+    client.close();
+    res.status(403).json({ message: "Wachtwoord is niet goed" });
+    return;
+  }
+
+  const hashedPassword = await hashPassword(newPassword);
+
+  usersCollection.updateOne(
+    {
+      email: userEmail,
+    },
+    {
+      $set: {
+        password: hashedPassword,
+      },
+    }
+  );
+
+  client.close();
+  res.status(200).json({ message: "Wachtwoord is geupdated" });
 }
 
 export default handler;
