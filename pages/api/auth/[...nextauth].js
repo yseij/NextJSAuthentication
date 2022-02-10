ff8bfd92b348e40904ed95d87f691714740f45a0
yseij <r0790156@student.thomasmore.be>
2022-02-10 14:35:21 +0100

    De Inloggegevens Auth Provider en gebruikersloginlogica toevoegen

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,41 @@
+import NextAuth from "next-auth";
+import Providers from "next-auth/providers";
+import { verifyPassword } from "../../../lib/auth";
+import { connectToDatabase } from "../../../lib/db";
+
+export default NextAuth({
+  sessios: {
+    jwt: true,
+  },
+  providers: [
+    Providers.Credentials({
+      async authorize(credentials) {
+        const client = await connectToDatabase();
+
+        const usersCollection = client.db().collection("users");
+
+        const user = await usersCollection.findOne({
+          email: credentials.email,
+        });
+
+        if (!user) {
+          client.close();
+          throw new Error("geen user gevonden");
+        }
+
+        const isValid = await verifyPassword(
+          credentials.password,
+          user.password
+        );
+
+        if (!isValid) {
+          client.close();
+          throw new Error("Kan niet inloggen");
+        }
+
+        client.close();
+        return { email: user.email };
+      },
+    }),
+  ],
+});
